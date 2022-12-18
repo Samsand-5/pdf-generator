34fedbb6bffde4458351b22e6b5bddd3d11747a3
coder <soumyodipsutra@gmail.com>
2022-12-18 09:23:46 +0530

    pdf generator

--- Hello.java
+++ Hello.java
@@ -1,5 +1,24 @@
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hatt bara");
+import java.util.*;
+import java.io.*;
+
+class codeforces{
+
+public static void main(){
+    Scanner sc=new Scanner(System.in);
+    int t=sc.nextInt();
+    while(t-->0){
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        int ans=0;
+        while(n>0){
+            int last=n&1;
+            ans=ans+last*k;
+            k=k*k;
+        }
+        int mod=(int)Math.pow(10.0,9.0);
+        System.out.println(ans%mod);
     }
 }
+}
+
+ 
